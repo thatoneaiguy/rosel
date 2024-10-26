742c4eb8d4825086167727b4d15fb8e4eb7fe09e
thatoneaiguy <animations.by.alby@gmail.com>
2024-10-26 16:29:59 +0000

    working on the rosel player shape crysta entity <3

--- src/main/java/com/thatoneaiguy/rosel/Rosel.java
+++ src/main/java/com/thatoneaiguy/rosel/Rosel.java
@@ -1,6 +1,7 @@
 package com.thatoneaiguy.rosel;
 
 import com.thatoneaiguy.rosel.cca.RoselCoatingComponent;
+import com.thatoneaiguy.rosel.entity.RoselCrystalPlayerShapeEntity;
 import com.thatoneaiguy.rosel.init.*;
 import com.thatoneaiguy.rosel.networking.RoselMessages;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
@@ -9,19 +10,29 @@ import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import eu.midnightdust.lib.config.MidnightConfig;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.entity.api.QuiltEntityTypeBuilder;
 import org.quiltmc.qsl.lifecycle.api.event.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Array;
 
 public class Rosel implements ModInitializer, EntityComponentInitializer {
 	public static final String MODID = "rosel";
@@ -70,6 +81,18 @@ public class Rosel implements ModInitializer, EntityComponentInitializer {
 		RoselMessages.registerC2S();
 
 		MidnightConfig.init("rosel", RoselConfig.class);
+
+		UseEntityCallback.EVENT.register((PlayerEntity player, World world, Hand hand, Entity entity, EntityHitResult hitResult) -> {
+			if ( entity instanceof RoselCrystalPlayerShapeEntity ) {
+				if ( !player.isHolding(RoselItems.ROSEL_SHARD)) {
+					return ActionResult.FAIL;
+				}
+				RoselCrystalPlayerShapeEntity.alive = true;
+
+				return ActionResult.CONSUME;
+			}
+			return ActionResult.FAIL;
+		});
 
 		ServerTickEvents.END.register(server -> {
 			if ( ticks > 0 ) {
--- src/main/java/com/thatoneaiguy/rosel/init/RoselEntities.java
+++ src/main/java/com/thatoneaiguy/rosel/init/RoselEntities.java
@@ -1,10 +1,29 @@
 package com.thatoneaiguy.rosel.init;
 
-/*public class RoselEntities {
-	public static final EntityType<DiscEntity> DiscEntityType = Registry.register(
+import com.thatoneaiguy.rosel.Rosel;
+import com.thatoneaiguy.rosel.entity.DiscEntity;
+import com.thatoneaiguy.rosel.entity.RoselCrystalPlayerShapeEntity;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class RoselEntities {
+//	public static final EntityType<DiscEntity> DiscEntityType = Registry.register(
+//		Registry.ENTITY_TYPE,
+//		new Identifier(Rosel.MODID, "disc"),
+//		FabricEntityTypeBuilder.<DiscEntity>create(SpawnGroup.MISC, DiscEntity::new)
+//			.spawnGroup(SpawnGroup.MISC)
+//			.dimensions(EntityDimensions.fixed(0.5F, 0.5F))
+//			.fireImmune()
+//			.trackRangeBlocks(4).trackedUpdateRate(10)
+//			.build());
+	public static final EntityType<RoselCrystalPlayerShapeEntity> ROSEL_CRYSTAL_PLAYER_SHAPE_ENTITY_TYPE = Registry.register(
 		Registry.ENTITY_TYPE,
 		new Identifier(Rosel.MODID, "disc"),
-		FabricEntityTypeBuilder.<DiscEntity>create(SpawnGroup.MISC, DiscEntity::new)
+		FabricEntityTypeBuilder.<RoselCrystalPlayerShapeEntity>create(SpawnGroup.MISC, RoselCrystalPlayerShapeEntity::new)
 			.spawnGroup(SpawnGroup.MISC)
 			.dimensions(EntityDimensions.fixed(0.5F, 0.5F))
 			.fireImmune()
@@ -14,4 +33,4 @@ package com.thatoneaiguy.rosel.init;
 	public static void register() {
 
 	}
-}*/
+}
