a2b0b67600549a52e31e22bd170fb128dc36f6a0
thatoneaiguy <animations.by.alby@gmail.com>
2024-12-22 17:11:27 +0000

    did some more model shit, laptops about to die and i forgot to brimg a charger. also updated mod bio and removed useless maven repo

--- src/main/java/com/thatoneaiguy/discharged/render/GauntletItemModel.java
+++ src/main/java/com/thatoneaiguy/discharged/render/GauntletItemModel.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class GauntletItemModel extends AnimatedGeoModel<RoselGauntletItem> {
-	private static final Identifier MODEL = new Identifier(Discharged.MODID, "geo/item/rat.geo.json");
+	private static final Identifier MODEL = new Identifier(Discharged.MODID, "geo/item/rosel_gauntlet_red-item.geo.json");
 	private static final Identifier DEFAULT_TEXTURE = new Identifier(Discharged.MODID, "textures/items/rosel_gauntlet_blue.png");
 	private static final Identifier ANIMATION = new Identifier(Discharged.MODID, "animations/item/rat.animation.json");
 
